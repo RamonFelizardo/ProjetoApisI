e17edcdc42e7fdc0af5b8cb2b36533a342d4a460
vinicius <viniciuscrato123@gmail.com>
2015-09-03 21:24:19 -0300

    commit vinicius

---
+++ src/main/java/br/com/pbldg/apis1/model/ViniciusBatista.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.pbldg.apis1.model;
+
+/**
+ *
+ * @author batista
+ */
+public class ViniciusBatista {
+    
+}
